2be3d0597e3ee876462678c6045f5ceb6245a561
unknown <themisbog@gmail.com>
2025-12-28 15:54:11 +0200

    Fixed buttons layout and made other changes

--- Calc.js
+++ Calc.js
@@ -8,6 +8,7 @@ function multNum(num1,num2){
     return num1*num2;
 }
 function divideNum(num1,num2){
+    if(num2 === 0) return "Error";
     return num1/num2;
 }
 let num1;
@@ -16,14 +17,14 @@ let operator;
 
 function operate(num1,operator,num2){
   if(operator==="+"){
-    addNum(num1,num2);
+    return addNum(num1,num2);
   }
   else if(operator==="-"){
-  subtractNum(num1,num2);
+  return subtractNum(num1,num2);
   }
   else if(operator==="*"){
-  multNum(num1,num2);}
+  return multNum(num1,num2);}
   else{
-    divideNum(num1,num2);
+    return divideNum(num1,num2);
   }
 }
